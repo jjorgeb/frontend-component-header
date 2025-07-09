9adfc99845719f32b3ae9f7293461ef86c645703
jjorgeb <jorge.barraza@uchile.cl>
2025-07-09 16:57:31 -0400

    test remove logo

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = ({
   ...attributes
 }) => (
   <a href={href} className="logo" {...attributes}>
-    <img className="d-block" src={src} alt={alt} />
+    <img className="d-block" src={'src'} alt={alt} />
   </a>
 );
 
